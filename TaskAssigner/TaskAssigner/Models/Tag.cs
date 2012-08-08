395a5520f80bf337aaa4d835a6b0f72eb1ebb6da
Chad Yeates <chad.yeates@gmail.com>
2012-08-08 09:08:04 -0500

    fixing many to many relationship between tickets > tags and developers > tags

--- TaskAssigner/TaskAssigner/Models/Tag.cs
+++ TaskAssigner/TaskAssigner/Models/Tag.cs
@@ -1,9 +1,14 @@
-﻿namespace TaskAssigner.Models
+﻿using System.Collections.Generic;
+
+namespace TaskAssigner.Models
 {
     public class Tag
     {
         public int TagId { get; set; }
         public string Name { get; set; }
+
+        public ICollection<Developer> Developers { get; set; }
+        public ICollection<Ticket> Tickets { get; set; } 
 
     }
 
